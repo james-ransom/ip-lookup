5a1dede12beafb3fc7b05e49774a7188ef08207f
james-ransom <ransom1538@protonmail.com>
2020-03-31 14:35:47 -0400

    adding in a class from IP2Location to lookup ip address information

---
+++ iplookup.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'IP2Location.php';
+$db = new \IP2Location\Database('IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE.BIN', \IP2Location\Database::FILE_IO);
+$ip=$_POST['ip'];
+$records = $db->lookup($ip, \IP2Location\Database::ALL);
+$ret = array(); 
+$fields = array('ipNumber', 'ipVersion', 'ipAddress', 'countryCode', 'countryName', 'regionName', 'cityName', 'latitude', 'longitude', 'zipCode');
+foreach($fields as $field) 
+{
+  $ret[$field] = $records[$field]; 
+}
+header('Content-Type: application/json');
+echo json_encode($ret); 
